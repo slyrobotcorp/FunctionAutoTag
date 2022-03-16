d5caa97a8391329c0fedd4f2daea8cc8b67b4447
Chris Wurtz <Chris.Wurtz@neudesic.com>
2022-03-16 10:36:50 -0600

    update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -13,8 +13,9 @@
 # Remove this if you are not planning on using MSI or Azure PowerShell.
 if ($env:MSI_SECRET) {
     Disable-AzContextAutosave -Scope Process | Out-Null
-    Connect-AzAccount -Identity -AccountId $env:AZURE_CLIENT_ID
+    Connect-AzAccount -Identity
 }
+
 
 # Uncomment the next line to enable legacy AzureRm alias in Azure PowerShell.
 # Enable-AzureRmAlias
